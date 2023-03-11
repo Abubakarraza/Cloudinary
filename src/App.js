88f210421aeed1943dc5504af2b901af2a18ad98
M Abubakar Raza <abubakarraza64@gmail.com>
2023-03-11 11:48:35 +0500

    cloudinary app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-const [image,setImage]=useState('');
-const [url,setUrl]=useState('');
-const onUploadImageHandler =()=>{
-  const data =new FormData();
-  data.append("file",image);
+  const [image, setImage] = useState('');
+  const [url, setUrl] = useState('');
+  const onUploadImageHandler = () => {
+    const data = new FormData();
+    data.append("file", image);
 
-  data.append("upload_preset","abubakarraza");
-  data.append("cloud_name","abubakarraza64");
+    data.append("upload_preset", "abubakarraza");
+    data.append("cloud_name", "abubakarraza64");
 
-  fetch("https://api.cloudinary.com/v1_1/abubakarraza64/image/upload",{
-    method:"post",
-    body:data
-  }).then(res => res.json()).
-  then((res) => {setUrl(res.url)
-  console.log(data);}).catch(err => console.log(err))
-}
+    fetch("https://api.cloudinary.com/v1_1/abubakarraza64/image/upload", {
+      method: "post",
+      body: data
+    }).then(res => res.json()).
+      then(res => setUrl(res.url)).
+      catch(err => console.log(err))
+  }
   return (
     <>
-    <input type="file" onChange={(e)=>setImage(e.target.files[0])}   />
-    <button onClick={onUploadImageHandler} style={{color:"goldenrod"}}>Upload</button>
-    {/* image will be displayed here */}
-    <img style={{height:"300px",width:"auto"}} src={url.toString()} alt="abuakar"  />
-    
+      <input type="file" onChange={(e) => setImage(e.target.files[0])} />
+      <button onClick={onUploadImageHandler} style={{ color: "goldenrod" }}>Upload</button>
+      {/* image will be displayed here */}
+      <img style={{ height: "300px", width: "auto" }} src={url.toString()} alt="abuakar" />
+
 
 
     </>
